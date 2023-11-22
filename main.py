3f80a4d4883296726fc59c6dc81c710e3be4206f
Calin <diaconuccalin@gmail.com>
2023-11-22 22:37:42 +0100

    Created script for making reports. Removed unnecessary variables. Added pre_dist variable. Added int_search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import time
 
 from src.io_stream import read_input_file, create_dzn
 
@@ -10,6 +12,7 @@ def main():
     :return: None
     """
 
+    """
     for el in os.listdir("data/problem_instances"):
         if el.endswith(".dat"):
             problem = read_input_file("data/problem_instances/" + el)
@@ -19,6 +22,42 @@ def main():
             print(len(problem.max_loads) != len(set(problem.max_loads)))
             print(len(problem.sizes) != len(set(problem.sizes)))
             print()
+    """
+
+    the_dir = "./data/CP/problem_instances"
+
+    max_report = 0
+    for el_2 in os.listdir("reports"):
+        el_2 = int(el_2[6:])
+        if el_2 > max_report:
+            max_report = el_2
+
+    with open("reports/report" + str(max_report + 1), "w") as fout:
+        for el in os.listdir(the_dir):
+            print(el)
+            data_path = os.path.join(the_dir, el)
+
+            p = subprocess.Popen(
+                ['minizinc', '--solver', 'gecode', './res/CP/the_problem.mzn', data_path],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+
+            to = 30
+            output = ""
+
+            try:
+                time_started = time.time()
+                output, _ = p.communicate(timeout=to)
+                time_delta = time.time() - time_started
+            except:
+                p.kill()
+                time_delta = to
+
+            inst_n = el[-6:-3]
+            fout.write(inst_n + " - R: " + output[output.rfind("dist > ") + len("dist > "):output.rfind("]")] + " - T: " + str(time_delta) + "\n")
 
     return None
 
