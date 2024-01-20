d7cd81fb76fdf9d6f961a55a74cc423741db8244
Calin <diaconuccalin@gmail.com>
2024-01-20 19:23:32 +0100

    Added Docker requirements. Clean up

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Start from MiniZinc base image
+FROM minizinc/minizinc
+
+# Install required packages
+RUN apt-get update
+RUN apt-get install -y python3 python3-pip git
+
+# Set working dir in the container
+WORKDIR /root
+
+# Clone repo
+RUN git clone https://github.com/diaconuccalin/CDMO_project.git
+RUN git switch calin
+
+# cd to project root
+cd CDMO_project/
+
+# Install python dependencies
+RUN pip3 install -r requirements.txt
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import time
 
-from src.io_stream import read_input_file, create_dzn, write_to_json
+from src.io_stream import write_to_json
 
 
 def main():
@@ -10,18 +10,6 @@ def main():
     The main function.
 
     :return: None
-    """
-
-    """
-    for el in os.listdir("data/problem_instances"):
-        if el.endswith(".dat"):
-            problem = read_input_file("data/problem_instances/" + el)
-            create_dzn(problem, "data/CP/problem_instances/" + el[:-4] + ".dzn")
-
-            print((problem.distances == problem.distances.T).all())
-            print(len(problem.max_loads) != len(set(problem.max_loads)))
-            print(len(problem.sizes) != len(set(problem.sizes)))
-            print()
     """
 
     the_dir = "./data/CP/problem_instances"
